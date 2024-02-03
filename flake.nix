1a4263a0c25b8c16af4ec91db5e2f56496c99bd4
Bert Proesmans <bproesmans@hotmail.com>
2024-02-03 23:01:11 +0000

    Additional development configuration

--- flake.nix
+++ flake.nix
@@ -161,6 +161,10 @@
               # Avoid copying unnecessary stuff over SSH
               nix.settings.builders-use-substitutes = true;
 
+              # Assist nix-direnv, since project devshells aren't rooted in the computer profile, nor stored in /nix/store
+              nix.settings.keep-outputs = true;
+              nix.settings.keep-derivations = true;
+
               # Make legacy nix commands consistent with flake sources!
               # Register versioned flake inputs into the nix registry for flake subcommands
               # Register versioned flake inputs as channels for nix (v2) commands
@@ -190,6 +194,10 @@
               services.vscode-server.enable = true;
               environment.systemPackages =
                 builtins.attrValues { inherit (pkgs) nixpkgs-fmt rnix-lsp; };
+
+              # Automatically load development shell in project working directories
+              programs.direnv.enable = true;
+              programs.direnv.nix-direnv.enable = true;
 
               # Disk and partition layout
               disko.devices = {
@@ -242,6 +250,34 @@
                   };
                 };
               };
+
+              # REF; https://github.com/nix-community/srvos/blob/bf8e511b1757bc66f4247f1ec245dd4953aa818c/nixos/common/networking.nix
+
+              # Networking configuration
+              # Allow PMTU / DHCP
+              networking.firewall.allowPing = true;
+
+              # Keep dmesg/journalctl -k output readable by NOT logging
+              # each refused connection on the open internet.
+              networking.firewall.logRefusedConnections = lib.mkDefault false;
+
+              # Use networkd instead of the pile of shell scripts
+              networking.useNetworkd = lib.mkDefault true;
+              networking.useDHCP = lib.mkDefault false;
+
+              # The notion of "online" is a broken concept
+              # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
+              systemd.services.NetworkManager-wait-online.enable = false;
+              systemd.network.wait-online.enable = false;
+
+              # FIXME: Maybe upstream?
+              # Do not take down the network for too long when upgrading,
+              # This also prevents failures of services that are restarted instead of stopped.
+              # It will use `systemctl restart` rather than stopping it with `systemctl stop`
+              # followed by a delayed `systemctl start`.
+              systemd.services.systemd-networkd.stopIfChanged = false;
+              # Services that are only restarted might be not able to resolve when resolved is stopped before
+              systemd.services.systemd-resolved.stopIfChanged = false;
 
               # Avoid TOFU MITM with github by providing their public key here.
               programs.ssh.knownHosts = {
