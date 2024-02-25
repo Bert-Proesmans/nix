c6ed6a4eda14a03571cfd14dafec55f167bed072
Bert Proesmans <bert.proesmans@e-powerinternational.com>
2024-02-25 09:28:06 +0100

    Restructured nixosModules directory
    This new structure maps directly onto the flake output schema

--- library/default.nix
+++
@@ -1,8 +0,0 @@
-{lib}:
-let
-  callLibs = file: import file { inherit lib; };
-  importers = callLibs ./importers.nix;
-in
-{
-    inherit (importers) rakeLeaves;
-}
--- modules/nixos/vscode-server.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, pkgs, ... }: {
-    imports = [ inputs.vscode-server.nixosModules.default ];
-
-    services.vscode-server.enable = true;
-
-    environment.systemPackages =
-        builtins.attrValues { inherit (pkgs) nixpkgs-fmt rnix-lsp; };
-}
