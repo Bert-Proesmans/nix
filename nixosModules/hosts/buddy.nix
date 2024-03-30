5442c228a6de97ddba3dc30cb6d5108489223ff1
Bert Proesmans <bproesmans@hotmail.com>
2024-03-30 20:53:26 +0000

    FS/ZFS config for buddy hardware

--- nixosModules/hosts/buddy.nix
+++ nixosModules/hosts/buddy.nix
@@ -4,11 +4,41 @@
   networking.domain = "alpha.proesmans.eu";
 
   proesmans.filesystem.simple-disk.enable = true;
-  proesmans.filesystem.simple-disk.device = "/dev/disk/by-id/ata-Samsung_SSD_850_EVO_120GB_S21UNSBG116809J";
+  proesmans.filesystem.simple-disk.device = "/dev/disk/by-id/nvme-INTEL_SSDPEKKW256G7_BTPY64630GRV256D";
   proesmans.nix.linux-64 = true;
   proesmans.nix.garbage-collect.enable = true;
   proesmans.internationalisation.be-azerty.enable = true;
   proesmans.home-manager.enable = true;
+
+  # Enables (nested) virtualization through hardware acceleration.
+  # There is no harm in having both modules loaded at the same time, also no real overhead.
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+
+  # Generated with `head -c4 /dev/urandom | od -A none -t x4`
+  # NOTE; The hostId is a marker that prevents ZFS from importing pools coming from another system.
+  # It's best practise to mark the pools as 'exported' before moving them between systems.
+  # NOTE; Force importing is possible, ofcourse.
+  networking.hostId = "525346fb";
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  # Leave ZFS pool alone!
+  boot.zfs.forceImportRoot = false;
+  boot.zfs.forceImportAll = false;
+
+  # services.zfs.autoSnapshot = {
+  #   enable = true;
+  #   # defaults to 12, which is a bit much given how much data is written
+  #   autoSnapshot.monthly = lib.mkDefault 1;
+  # };
+
+  # services.zfs.autoScrub = {
+  #   enable = true;
+  #   pools = [ "tank" ];
+  #   interval = "weekly";
+  # };
+
+  services.fstrim.enable = true;
+  #services.zfs.trim.enable = true;
 
   # Make me a user!
   users.users.bert-proesmans = {
