6220d85d04365102bdc0e754cbeb5072d8a376df
Bert Proesmans <bproesmans@hotmail.com>
2024-09-29 12:49:53 +0000

    Put unsock nginx fixes behind option

--- nixosModules/unsock-nginx.nix
+++ nixosModules/unsock-nginx.nix
@@ -2,21 +2,29 @@
 let
   # ERROR; Infinite recursion
   # any-unsock-enabled = builtins.any (v: v == true) (lib.mapAttrsToList (_: v: v.unsock.enable) config.systemd.services);
+  cfg = config.proesmans.fixes.unsock-nginx;
 in
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      # Do this for all nginx packages, why are there so many .. 
-      nginxStable = prev.nginxStable.overrideAttrs (old: {
-        # Forcefully add poll module for event handling. This method can be used with UNSOCK
-        configureFlags = old.configureFlags ++ [ "--with-poll_module" ];
-      });
-    })
-  ];
 
-  services.nginx.eventsConfig = ''
-    # ERROR; epoll event method doesn't work with socket rebind
-    # REF; https://github.com/kohlschutter/unsock/issues/2#issuecomment-2380074418
-    use poll;
-  '';
+  options.proesmans.fixes.unsock-nginx = {
+    enable = lib.mkEnableOption "unsock'able nginx";
+  };
+
+  config = lib.mkIf cfg.enable {
+    nixpkgs.overlays = [
+      (final: prev: {
+        # Do this for all nginx packages, why are there so many .. 
+        nginxStable = prev.nginxStable.overrideAttrs (old: {
+          # Forcefully add poll module for event handling. This method can be used with UNSOCK
+          configureFlags = old.configureFlags ++ [ "--with-poll_module" ];
+        });
+      })
+    ];
+
+    services.nginx.eventsConfig = ''
+      # ERROR; epoll event method doesn't work with socket rebind
+      # REF; https://github.com/kohlschutter/unsock/issues/2#issuecomment-2380074418
+      use poll;
+    '';
+  };
 }
