987d2d50d2c3220bee8ede2f34126baf6263f054
Bert Proesmans <bproesmans@hotmail.com>
2024-12-29 21:03:25 +0000

    Simplified module "filesystem"

--- nixosModules/filesystem.nix
+++ nixosModules/filesystem.nix
@@ -1,3 +1,4 @@
+# Simple hardware and disk configuration for physicl/virtual machines
 { lib, special, config, options, ... }:
 let
   cfg = config.proesmans.filesystem;
@@ -12,80 +13,75 @@ in
       type = lib.types.str;
       description = lib.mdDoc "The name of the one disk to format";
     };
-    simple-disk.systemd-boot.enable = lib.mkEnableOption (lib.mdDoc "Use the systemd bootloader");
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf cfg.simple-disk.enable {
-      # Cleaning tmp directory not required if it's a tmpfs
-      # Enabling tmpfs for tmp also prevents additional SSD writes
-      boot.tmp.cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
-    })
-    (lib.mkIf (cfg.simple-disk.enable && cfg.simple-disk.systemd-boot.enable) {
-      # EFI boot!
-      boot.loader.systemd-boot.enable = lib.mkDefault true;
-      boot.loader.systemd-boot.editor = false;
-    })
-    (lib.mkIf cfg.simple-disk.enable {
-      assertions = [{
-        assertion = builtins.stringLength cfg.simple-disk.device > 0;
-        message = ''
-          A device path must be provided for formatting to work!
-          Set one at 'proesmans.filesystem.simple-disk.device'.
-        '';
-      }];
+  config = lib.mkIf cfg.simple-disk.enable {
+    assertions = [{
+      assertion = builtins.stringLength cfg.simple-disk.device > 0;
+      message = ''
+        A device path must be provided for formatting to work!
+        Set one at 'proesmans.filesystem.simple-disk.device'.
+      '';
+    }];
 
-      disko.devices = {
-        disk.disk1 = {
-          device = cfg.simple-disk.device;
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                type = "EF00";
-                size = "500M";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
-              };
-              encryptedSwap = {
-                size = "2G";
-                content = {
-                  type = "swap";
-                  randomEncryption = true;
-                };
-              };
-              root = {
-                size = "100%";
-                content = {
-                  type = "lvm_pv";
-                  vg = "pool";
-                };
-              };
+    # EFI boot!
+    boot.loader.systemd-boot.enable = lib.mkDefault true;
+    boot.loader.systemd-boot.editor = lib.mkDefault false;
 
+    # Cleaning tmp directory not required if it's a tmpfs
+    # Enabling tmpfs for tmp also prevents additional SSD writes
+    boot.tmp.cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
+
+    disko.devices = {
+      disk.root = {
+        device = cfg.simple-disk.device;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
+            encryptedSwap = {
+              size = "2G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
+            };
+
           };
         };
-        lvm_vg = {
-          pool = {
-            type = "lvm_vg";
-            lvs = {
-              root = {
-                size = "100%FREE";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                  mountOptions = [ "defaults" ];
-                };
+      };
+      lvm_vg = {
+        pool = {
+          type = "lvm_vg";
+          lvs = {
+            root = {
+              size = "100%FREE";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                mountOptions = [ "defaults" ];
               };
             };
           };
         };
       };
-    })
-  ];
+    };
+  };
 }
