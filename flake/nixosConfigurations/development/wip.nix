6fbd4ce40f77ffec3f86aa47770fe8daf4b77ea5
Bert Proesmans <bproesmans@hotmail.com>
2025-05-01 22:20:41 +0000

    Make crowdsec work

--- flake/nixosConfigurations/development/wip.nix
+++ flake/nixosConfigurations/development/wip.nix
@@ -1,8 +1,17 @@
 { lib, flake, pkgs, config, ... }:
+let
+  # ERROR; Upstream does not handle deviating from the default URI !!
+  crowdsecAPI = "127.0.0.1:8080";
+  bouncerKey = "cs-firewall-bouncer-key";
+in
 {
+  imports = [
+    flake.inputs.crowdsec.nixosModules.crowdsec
+    flake.inputs.crowdsec.nixosModules.crowdsec-firewall-bouncer
+  ];
+
   sops.secrets.crowdsec-apikey.owner = "crowdsec";
 
-  imports = [ flake.inputs.crowdsec.nixosModules.crowdsec ];
   services.crowdsec =
     let
       # NOTE; Derived from upstream module config
@@ -13,34 +22,125 @@
       enrollKeyFile = config.sops.secrets.crowdsec-apikey.path;
       allowLocalJournalAccess = true;
 
-      acquisitions = [{
-        source = "journalctl";
-        journalctl_filter = [ "_SYSTEMD_UNIT=sshd.service" ];
-        labels.type = "syslog";
-      }];
+      acquisitions = [
+        ({
+          source = "journalctl";
+          journalctl_filter = [ "_SYSTEMD_UNIT=sshd.service" ];
+          labels.type = "syslog";
+        })
+        ({
+          source = "journalctl";
+          journalctl_filter = [ "_SYSTEMD_UNIT=nginx.service" ];
+          labels.type = "syslog";
+        })
+      ];
 
       settings = {
-        api.server.listen_uri = "127.0.0.1:8080";
+        api.server.listen_uri = crowdsecAPI;
         prometheus.enabled = false;
+        cscli.output = "human";
         config_paths = {
           # Setup a R/W path to dynamically enable/disable simulations.
           # SEEALSO; systemd.services.crowdsec.serviceConfig.ExecStartPre
           simulation_path = "${configDir}/simulation.yaml";
         };
-        nftables = {
-          ipv4 = {
-            enabled = false;
-            table = "firewall"; # Match networking.nftables.tables.<name>
-            set-only = true;
-          };
-          ipv6 = {
-            enabled = false;
-            table = "firewall"; # Match networking.nftables.tables.<name>
-            set-only = true;
-          };
+      };
+    };
+
+  systemd.services.crowdsec.serviceConfig = {
+    ExecStartPre =
+      let
+        installConfigurations = pkgs.writeShellApplication {
+          name = "install-configurations";
+          # ERROR; crowdsec cli tool is wrapped with setting arguments, we need those!
+          runtimeInputs = config.systemd.services.crowdsec.path;
+          text = ''
+            # WARN; Required on first run to hydrate the hub index
+            # Is executed by the upstream ExecStartPre script!
+            # cscli hub upgrade
+
+            ## Collections
+            cscli collections install \
+              crowdsecurity/linux \
+              crowdsecurity/nginx
+
+            ## Parsers
+            # Whitelists private IPs
+            # if ! cscli parsers list | grep -q "whitelists"; then
+            #     cscli parsers install crowdsecurity/whitelists
+            # fi
+
+            ## Heavy operations
+            cscli postoverflows install \
+              crowdsecurity/ipv6_to_range \
+              crowdsecurity/rdns
+
+            ## Report-only (no action taken) scenario's
+            echo 'simulation: false' >'${config.services.crowdsec.settings.config_paths.simulation_path}'
+            cscli simulation enable crowdsecurity/http-bad-user-agent
+            cscli simulation enable crowdsecurity/http-crawl-non_statics
+            cscli simulation enable crowdsecurity/http-probing
+          '';
+        };
+      in
+      lib.mkAfter [ (lib.getExe installConfigurations) ];
+    ExecStartPost =
+      let
+        waitForStart = pkgs.writeShellApplication {
+          name = "wait-for-start";
+          # ERROR; crowdsec cli tool is wrapped with setting arguments, we need those!
+          runtimeInputs = config.systemd.services.crowdsec.path ++ [ pkgs.coreutils ];
+          text = ''
+            while ! nice -n19 cscli lapi status; do
+              echo "Waiting for CrowdSec daemon to be ready"
+              sleep 10
+            done
+          '';
+        };
+
+        bouncers = pkgs.writeShellApplication {
+          name = "setup-bouncers";
+          # ERROR; crowdsec cli tool is wrapped with setting arguments, we need those!
+          runtimeInputs = config.systemd.services.crowdsec.path ++ [ pkgs.coreutils ];
+          text = ''
+            # LOCAL bouncer
+            if ! cscli bouncers list | grep -q "local-bouncer"; then
+              cscli bouncers add "local-bouncer" --key "${bouncerKey}"
+            fi
+          '';
+        };
+      in
+      lib.mkMerge [
+        (lib.mkBefore [ (lib.getExe waitForStart) ])
+        (lib.mkAfter [ (lib.getExe bouncers) ])
+      ];
+  };
+
+  # WARN; Package "crowdsec-firewall-bouncer" does not exist upstream, need to overlay the package index!
+  proesmans.nix.overlays = [ flake.inputs.crowdsec.overlays.default ];
+
+  services.crowdsec-firewall-bouncer = {
+    enable = true;
+    settings = {
+      api_key = bouncerKey;
+      api_url = "http://${crowdsecAPI}";
+      log_mode = "stdout";
+      update_frequency = "10s";
+      mode = "nftables";
+      nftables = {
+        ipv4 = {
+          enabled = true;
+          table = "firewall"; # Match networking.nftables.tables.<name>
+          set-only = true;
+        };
+        ipv6 = {
+          enabled = true;
+          table = "firewall"; # Match networking.nftables.tables.<name>
+          set-only = true;
         };
       };
     };
+  };
 
   networking.nftables.enable = true;
   networking.nftables.tables = {
@@ -50,24 +150,35 @@
     firewall = {
       family = "inet"; # ipv4/6 combined
       content = ''
-        # NOTE; set name is hardcoded
+        # WARN; set name is hardcoded
+        # See contents with; nft list set inet firewall crowdsec-blacklists
         set crowdsec-blacklists {
+          comment "IPs blocked by crowdsec"
           type ipv4_addr
           flags timeout
         }
 
-        # NOTE; set name is hardcoded
+        # WARN; set name is hardcoded
+        # See contents with; nft list set inet firewall crowdsec6-blacklists
         set crowdsec6-blacklists {
+          comment "IPs blocked by crowdsec"
           type ipv6_addr
           flags timeout
         }
 
-        chain bouncer {
+        chain bouncer-in {
           # Hook as early as possible to keep packet processing load light.
-          type filter hook prerouting priority raw; policy accept;
+          type filter hook input priority raw; policy accept;
 
-          ip saddr @crowdsec-blacklists drop
-          ip6 saddr @crowdsec6-blacklists drop
+          ip saddr @crowdsec-blacklists drop comment "see nft list set inet firewall crowdsec-blacklists"
+          ip6 saddr @crowdsec6-blacklists drop comment "see nft list set inet firewall crowdsec6-blacklists"
+        }
+
+        chain bouncer-out {
+          type filter hook output priority raw; policy accept;
+
+          ip daddr @crowdsec-blacklists drop comment "see nft list set inet firewall crowdsec-blacklists"
+          ip6 daddr @crowdsec6-blacklists drop comment "see nft list set inet firewall crowdsec6-blacklists"
         }
 
         chain rpfilter {
@@ -132,58 +243,5 @@
         }
       '';
     };
-  };
-
-  systemd.services.crowdsec.serviceConfig = {
-    ExecStartPre =
-      let
-        installConfigurations = pkgs.writeShellApplication {
-          name = "install-configurations";
-          # ERROR; crowdsec cli tool is wrapped with setting arguments, we need those!
-          runtimeInputs = config.systemd.services.crowdsec.path;
-          text = ''
-            # WARN; Required on first run to hydrate the hub index
-            # Is executed by the upstream ExecStartPre script!
-            # cscli hub upgrade
-
-            ## Collections
-            cscli collections install \
-              crowdsecurity/linux
-
-            ## Parsers
-            # Whitelists private IPs
-            # if ! cscli parsers list | grep -q "whitelists"; then
-            #     cscli parsers install crowdsecurity/whitelists
-            # fi
-
-            ## Heavy operations
-            cscli postoverflows install \
-              crowdsecurity/ipv6_to_range \
-              crowdsecurity/rdns
-
-            ## Non-actionable scenario's
-            echo 'simulation: false' >'${config.services.crowdsec.settings.config_paths.simulation_path}'
-            cscli simulation enable crowdsecurity/http-bad-user-agent
-            cscli simulation enable crowdsecurity/http-crawl-non_statics
-            cscli simulation enable crowdsecurity/http-probing
-          '';
-        };
-      in
-      lib.mkAfter [ (lib.getExe installConfigurations) ];
-    ExecStartPost =
-      let
-        waitForStart = pkgs.writeShellApplication {
-          name = "wait-for-start";
-          # ERROR; crowdsec cli tool is wrapped with setting arguments, we need those!
-          runtimeInputs = config.systemd.services.crowdsec.path ++ [ pkgs.coreutils ];
-          text = ''
-            while ! nice -n19 cscli lapi status; do
-              echo "Waiting for CrowdSec daemon to be ready"
-              sleep 10
-            done
-          '';
-        };
-      in
-      lib.mkBefore [ (lib.getExe waitForStart) ];
   };
 }
