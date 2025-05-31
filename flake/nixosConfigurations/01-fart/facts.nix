7dc5197a06d4796e10eacb600b8fb10d1412ac5f
Bert Proesmans <bproesmans@hotmail.com>
2025-05-31 20:09:39 +0000

    Update facts for 01-fart

--- flake/nixosConfigurations/01-fart/facts.nix
+++ flake/nixosConfigurations/01-fart/facts.nix
@@ -1,7 +1,9 @@
 { ... }: {
-  ipAddress = "144.21.42.246";
+  ipAddress = "158.101.202.58";
+  # IPv6 not handled currently
   domainName = "omega.proesmans.eu";
   tags = [
     "fart"
   ];
+  encryptedDisks = true;
 }
