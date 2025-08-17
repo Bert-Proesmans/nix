2a5a89bfc2a3e6487654044e8da209f0e5330889
Bert Proesmans <bproesmans@hotmail.com>
2025-08-17 14:34:29 +0000

    Enable zram on 01-fart

--- flake/nixosConfigurations/01-fart/memory-handling.nix
+++ flake/nixosConfigurations/01-fart/memory-handling.nix
@@ -18,13 +18,13 @@
     "boot.shell_on_fail" # DEBUG
   ];
 
-  # boot.kernel.sysctl = {
-  #   # REF; https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
-  #   "vm.swappiness" = 200;
-  #   "vm.page-cluster" = 0;
-  #   "vm.watermark_boost_factor" = 0;
-  #   "vm.watermark_scale_factor" = 125;
-  # };
+  boot.kernel.sysctl = {
+    # REF; https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+    "vm.swappiness" = 200;
+    "vm.page-cluster" = 0;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+  };
 
   systemd.oomd = {
     # Newer iteration of earlyoom!
@@ -42,7 +42,7 @@
     # ERROR; Managing least-recently-used (LRU) inside ZRAM will improve latency, but this isn't how the mechanism exactly works either.
     # The writeback device will receive _randomly_ chosen 'idle' pages, causing high variance in latency! There is a configurable access
     # timer, however, that marks pages as idle automatically.
-    enable = false; # DEBUG
+    enable = true;
     # NOTE; Refer to this swap device by "/sys/block/zram0"
     swapDevices = 1;
     memoryMax = 2 * 1024 * 1024 * 1024; # (2GB) Bytes, total size of swap device aka max size of uncompressed data
@@ -52,7 +52,7 @@
   };
 
   systemd.services."zram0-maintenance" = {
-    enable = false; # DEBUG
+    enable = true;
     description = "Maintain zram0 data";
     startAt = "*-*-* 00/1:00:00"; # Every hour
     requisite = [ "systemd-zram-setup@zram0.service" ];
@@ -60,16 +60,22 @@
     serviceConfig.RemainAfterExit = "no";
     enableStrictShellChecks = true;
     script = ''
+      # ERROR; Does NOT work without CONFIG_ZRAM_TRACK_ENTRY_ACTIME ! See below for alternative
+      # Mark all pages older than provided seconds as idle.
+      # REF; https://docs.kernel.org/admin-guide/blockdev/zram.html#writeback
+      # echo "14400" > /sys/block/zram0/idle # 4 hours
+
+
       # NOTE; The zram device _does not_ automatically manage least-recently-used (LRU) eviction!
       # ERROR; Out-of-the-box Linux kernel in NixOS is _not configured_ with CONFIG_ZRAM_TRACK_ENTRY_ACTIME! There is no idle time
       # tracking on zram pages, so this impacts how we handle writeback of idle pages!
       #
-      # Without idle-marking, we'll move all pages marked idle during the previous execution of this script.
-      # 1. Writeback marked pages
+      # Without automatic idle-marker tracking, we'll do this ourselves in a two step process.
+      # 1. Writeback/evict marked pages
       # 2. Mark all pages currently stored as idle
       #   - Pages' idle mark will be removed on retrieval/set
 
-      # Evict pages from RAM. [ONE-TIME]
+      # Evict pages from RAM.
       #
       # [huge] Write incompressible page clusters(?) to backing device
       # [idle] Write idle pages to backing device
@@ -81,15 +87,9 @@
       # SEEALSO; boot.kernel.sysctl."vm.page-cluster"
       echo "huge_idle" > /sys/block/zram0/writeback
 
-      # Mark all pages as idle. [ONE-TIME]
+      # Mark all remaining pages as idle.
       # REF; https://docs.kernel.org/admin-guide/blockdev/zram.html#writeback
       echo "all" > /sys/block/zram0/idle
-
-      # ERROR; Does NOT work without CONFIG_ZRAM_TRACK_ENTRY_ACTIME !
-      # Mark all pages older than provided seconds as idle. [ONE-TIME]
-      # = 4 hours
-      # REF; https://docs.kernel.org/admin-guide/blockdev/zram.html#writeback
-      # echo "14400" > /sys/block/zram0/idle
     '';
   };
 }
