ab470346316457a86844d733d60c8d6e1169ad98
Bert Proesmans <bproesmans@hotmail.com>
2025-10-06 12:47:10 +0000

    Add tftpd script for vps rescue

--- flake/nixosConfigurations/01-fart/configuration.nix
+++ flake/nixosConfigurations/01-fart/configuration.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   ...
 }:
 {
@@ -43,6 +44,41 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEeQ/KEIWbUKBc4bhZBUHsBB0yJVZmBuln8oSVrtcA5 bert@B-PC"
     ];
   };
+
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "rescue-tftpd";
+      runtimeInputs = [
+        pkgs.coreutils
+        pkgs.rs-tftpd
+      ];
+      text = ''
+        # NOTE; Provide .efi executables to rescue other virtual machines in the VPS
+        #
+        # The images below are specific to Oracle cloud.
+        # curl -L https://github.com/netbootxyz/netboot.xyz/releases/latest/download/netboot.xyz-arm64.efi -o netboot.xyz-arm64.efi
+        # curl -L https://github.com/netbootxyz/netboot.xyz/releases/latest/download/netboot.xyz-snp.efi -o netboot.xyz-snp.efi
+
+        SRC_DIR=$(mktemp -d)
+        PORT="6969"
+
+        echo "Launching TFTP daemon serving files from directory: $SRC_DIR"
+
+        # Open firewall with;
+        # sudo iptables -I INPUT 1 -s 10.0.84.0/24 -p tcp --dport 6969 -j ACCEPT
+        # sudo iptables -I INPUT 1 -s 10.0.84.0/24 -p udp --dport 6969 -j ACCEPT
+        #
+        # Verify with;
+        # sudo iptables -L INPUT -n --line-numbers
+        #
+        # NOTE; The above commands do not persist across a reboot!
+        echo "WARNING; Make sure to allow incoming connections on port $PORT"
+
+        # WARN; Must bind to port >1024 to allow user-bind
+        tftpd --ip-address 0.0.0.0 --port "$PORT" --directory "$SRC_DIR" --read-only
+      '';
+    })
+  ];
 
   # Ignore below
   # Consistent defaults accross all machine configurations.
