06ad7d185dd832dd211974e0a62256ed0f4e37e8
Bert Proesmans <bproesmans@hotmail.com>
2025-12-18 23:36:40 +0000

    Apply profile remote-machine to fart hosts

--- flake/nixosConfigurations/01-fart/configuration.nix
+++ flake/nixosConfigurations/01-fart/configuration.nix
@@ -9,6 +9,7 @@
     ./disks.nix
     ./hardware-configuration.nix
     flake.profiles.omega-loadbalancer
+    flake.profiles.remote-machine
   ];
 
   # Slows down write operations considerably
@@ -27,19 +28,6 @@
   security.sudo.enable = true;
   # Allow for passwordless sudo
   security.sudo.wheelNeedsPassword = false;
-
-  # Make me an admin!
-  users.users.bert-proesmans = {
-    isNormalUser = true;
-    description = "Bert Proesmans";
-    extraGroups = [
-      "wheel" # Allows sudo access
-      "systemd-journal" # Read the systemd service journal without sudo
-    ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEeQ/KEIWbUKBc4bhZBUHsBB0yJVZmBuln8oSVrtcA5 bert@B-PC"
-    ];
-  };
 
   environment.systemPackages = [
     (pkgs.writeShellApplication {
--- flake/nixosConfigurations/02-fart/configuration.nix
+++ flake/nixosConfigurations/02-fart/configuration.nix
@@ -8,6 +8,7 @@
     ./disks.nix
     ./hardware-configuration.nix
     flake.profiles.omega-loadbalancer
+    flake.profiles.remote-machine
   ];
 
   # Slows down write operations considerably
@@ -26,19 +27,6 @@
   security.sudo.enable = true;
   # Allow for passwordless sudo
   security.sudo.wheelNeedsPassword = false;
-
-  # Make me an admin!
-  users.users.bert-proesmans = {
-    isNormalUser = true;
-    description = "Bert Proesmans";
-    extraGroups = [
-      "wheel" # Allows sudo access
-      "systemd-journal" # Read the systemd service journal without sudo
-    ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEeQ/KEIWbUKBc4bhZBUHsBB0yJVZmBuln8oSVrtcA5 bert@B-PC"
-    ];
-  };
 
   # Ignore below
   # Consistent defaults accross all machine configurations.
