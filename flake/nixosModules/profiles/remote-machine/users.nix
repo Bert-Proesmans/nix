d25638bd33f2532948fbe2855e5cfd1ed2d60c8b
Bert Proesmans <bproesmans@hotmail.com>
2026-01-02 13:02:44 +0000

    Add additional ssh key to remote-machine profile

--- flake/nixosModules/profiles/remote-machine/users.nix
+++ flake/nixosModules/profiles/remote-machine/users.nix
@@ -10,6 +10,7 @@
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEeQ/KEIWbUKBc4bhZBUHsBB0yJVZmBuln8oSVrtcA5 bert@B-PC"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQ6i6epTE7G73/fZT1V5iBIEwBS/mpMoOfv3OOo+cMr azuread\\bertproesmans@epower-518172"
     ];
   };
 }
