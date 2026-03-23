995827a0010138f623948bee2a190af7160b5aa5
Bert Proesmans <bproesmans@hotmail.com>
2026-03-23 21:08:10 +0000

    Allow insecure varnish 7.7.3

--- flake/nixosModules/profiles/omega-loadbalancer/default.nix
+++ flake/nixosModules/profiles/omega-loadbalancer/default.nix
@@ -9,4 +9,10 @@
     ./web-cache.nix
     ./web-security.nix
   ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    # NOTE; Need to upgrade to varnish 8.0.1
+    # REF; VSV00018: https://vinyl-cache.org/security/VSV00018.html
+    "varnish-7.7.3"
+  ];
 }
