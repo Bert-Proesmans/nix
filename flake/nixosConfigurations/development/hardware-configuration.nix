1e69e898e74089fee98c58d85781590d3de3b91c
Bert Proesmans <bproesmans@hotmail.com>
2026-05-29 11:57:29 +0200

    Unify development host network details

--- flake/nixosConfigurations/development/hardware-configuration.nix
+++ flake/nixosConfigurations/development/hardware-configuration.nix
@@ -44,40 +44,19 @@
   # eth1 gets a stable link-local address for SSH, because Windows goes fucky wucky with
   # the host bridge network adapter and that's sad because IP's and routes won't stick
   # after a reboot.
-  systemd.network.links = {
-    "10-upstream" = {
-      matchConfig.OriginalName = "eth0";
-      linkConfig.Alias = "Internet uplink";
-      linkConfig.AlternativeName = "main";
-    };
-    "10-hypervisor-connect" = {
-      matchConfig.OriginalName = "eth1";
-      linkConfig.Alias = "Link local management";
-    };
-  };
-
-  systemd.network.networks = {
-    "30-upstream" = {
-      # ERROR; Don't forget to enable MAC address spoofing on the VM network interface
-      # attached to host adapter "Static Net"!
-      # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Applicable if using bridged networking on guest, not if NAT'ing
+   systemd.network.networks = {
+    "eth0-management" = {
       matchConfig.Name = "eth0";
-      networkConfig.DHCP = "ipv4";
-      networkConfig.LinkLocalAddressing = "no";
-      # ERROR; Hyper-V managed default switch provides a DNS IP that doesn't resolve
-      # Fucking broken Windows and Hyper-V bullshit
-      dhcpV4Config.UseDNS = false;
-    };
-
-    "30-hypervisor-connect" = {
-      matchConfig.Name = "eth1";
-      networkConfig = {
-        Address = [
-          "169.254.245.139/24"
-          "fe80::139/64"
-        ];
-        LinkLocalAddressing = "no";
-      };
+      DHCP = "yes";
+      # NOTE; Address configuration matches VM configuration!
+      address = [
+        "172.27.224.139/24"
+        "fde0:5584:ba8e::139/64"
+      ];
+      gateway = [
+        "172.27.224.1"
+        "fde0:5584:ba8e::1"
+      ];
     };
   };
 }
