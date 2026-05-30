bfe2ca39b859f93d1bc2707029775a8c65010ad8
Bert Proesmans <bproesmans@hotmail.com>
2026-05-30 09:50:07 +0000

    Update nixpkgs stable reference

--- flake/flake.nix
+++ flake/flake.nix
@@ -6,7 +6,7 @@
 
   inputs = {
     nixpkgs.follows = "nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # WARN; Seems like everybody is using flake-utils, but this dependency does not bring anything
